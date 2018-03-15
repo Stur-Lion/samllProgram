e66611fb634ff6f37dfbc3577c0a77e1261c0ce4
cckings <1504468447@qq.com>
2018-03-15 18:38:28 +0800

    shen

---
+++ shen/pages/index/index.js
@@ -0,0 +1,17 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    years:[1,2,3,4],
+    months:[1,2,3,4],
+    days:[1,2,3,4]
+  },
+  onLoad: function () {
+
+  },
+  getUserInfo: function(e) {
+
+  }
+})
